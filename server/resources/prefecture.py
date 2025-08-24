e0b62766a213f67439e2c996a6394ff585db0a2d
Kaan Buke <kabuke13@gmail.com>
2025-08-24 15:00:02 +0200

    allows user to add a prefecture to the database

--- server/resources/prefecture.py
+++ server/resources/prefecture.py
@@ -9,3 +9,19 @@ class PrefectureList(Resource):
     def get(self):
         prefectures = [prefecture.to_dict() for prefecture in PrefectureModel.query.all()]
         return prefectures, 200
+    
+    def post(self):
+        json = request.get_json()
+
+        try:
+            new_prefecture = PrefectureModel(
+                name=json.get("name"),
+                img=json.get("img"),
+                capital=json.get("capital"),
+                intro=json.get("intro")
+            )
+            db.session.add(new_prefecture)
+            db.session.commit()
+            return new_prefecture.to_dict(), 201
+        except ValueError as e:
+            return {"message": [str(e)]}, 404
