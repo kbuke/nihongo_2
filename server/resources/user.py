c848f7b6dae0a1550bc61192344c1d51d64ac33c
Kaan Buke <kabuke13@gmail.com>
2025-08-25 10:37:17 +0200

    add extra validation steps for new users email address

--- server/resources/user.py
+++ server/resources/user.py
@@ -7,6 +7,8 @@ from config import db
 from flask_restful import Resource
 from flask import session, make_response, request
 
+import re
+
 class UserList(Resource):
     def get(self):
         users = [user.to_dict() for user in UserModel.query.all()]
@@ -17,28 +19,36 @@ class UserList(Resource):
 
         account_type = json.get("ac_type")
 
-        city_id = json.get("city_id")
-        city = CityModel.query.filter(CityModel.id==city_id).first()
-        print(city)
+        # handle logic for registered email
+        new_email = json.get("email")
+        pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+        if pattern.match(new_email):
+            new_email=new_email
+        else:
+            return{"message": "email address is not valid."}, 422
 
-        if account_type=="business" and city:
-            new_business = BusinessModel(
-                email=json.get("email"),
-                ac_type=account_type,
-                name=json.get("name"),
-                postal_code=json.get("postal_code"),
-                district_block=json.get("district_block"),
-                building_name=json.get("building_name"),
-                floor_room=json.get("floor_room"),
-                allow_email=json.get("allow_email"),
-                city_id=city_id
-            )
-            db.session.add(new_business)
-            db.session.commit()
-            return new_business.to_dict(), 201
-        
-        elif city==None:
-            return{"error": f"No city with id: {city_id}"}
+        if account_type=="business":
+            city_id = json.get("city_id")
+            city = CityModel.query.filter(CityModel.id==city_id).first()
+
+            if city:
+                new_business = BusinessModel(
+                    email=new_email,
+                    ac_type=account_type,
+                    name=json.get("name"),
+                    postal_code=json.get("postal_code"),
+                    district_block=json.get("district_block"),
+                    building_name=json.get("building_name"),
+                    floor_room=json.get("floor_room"),
+                    allow_email=json.get("allow_email"),
+                    city_id=city_id
+                )
+                db.session.add(new_business)
+                db.session.commit()
+                return new_business.to_dict(), 201
+            
+            else:
+                return{"error": f"No city with id: {city_id}"}
         
         elif account_type=="individual":
             new_business = IndividualModel(
@@ -54,26 +64,3 @@ class UserList(Resource):
         else:
             return {"message": "Please enter a proper account type"}, 401
     
-# class BusinessList(Resource):
-#     def post(self):
-#         json = request.get_json()
-        
-#         account_type = json.get("ac_type")
-
-#         if account_type=="business":
-#             new_business = BusinessModel(
-#                 email=json.get("email"),
-#                 ac_type=account_type,
-#                 name=json.get("name"),
-#                 postal_code=json.get("postal_code"),
-#                 district_block=json.get("district_block"),
-#                 building_name=json.get("building_name"),
-#                 floor_room=json.get("floor_room"),
-#                 allow_email=json.get("allow_email"),
-#                 city_id=json.get("city_id")
-#             )
-#             db.session.add(new_business)
-#             db.session.commit()
-#             return new_business.to_dict(), 201
-#         else:
-#             return{"message": "User is not a business. Sign up failed"}
