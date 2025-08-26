1aa1de9c7821250c2b898705dc59deb5280dce43
Kaan Buke <kabuke13@gmail.com>
2025-08-26 10:40:03 +0200

    add validation to process of creating a new wishlist

--- server/resources/wishlist.py
+++ server/resources/wishlist.py
@@ -16,24 +16,53 @@ class WishlistList(Resource):
         json = request.get_json()
 
         user_id = json.get("user_id")
-        user_info = IndividualModel.query.filter(IndividualModel.id == user_id).first()
-        if user_info:
-            user_info = user_info.to_dict()
+        user_info = IndividualModel.query.filter(IndividualModel.id==user_id).first()
+        user_info = user_info.to_dict()
+
+        existing_wishlists = [wishlist["name"] for wishlist in user_info["wishlists"]]
+        print(existing_wishlists)
+
+        wishlist_name = f"{json.get('name')} Prefecture"
+
+        if wishlist_name in existing_wishlists:
+            return{"error": f"This user has already created a wishlist for {wishlist_name}"}, 422
         else:
-            return {"error": "No user found"}
-        print(user_info)
+            wishlist_name=wishlist_name
 
         try:
             new_wishlist = WishlistModel(
-                name=json.get("name"),
+                name=wishlist_name,
                 img=json.get("img"),
                 description=json.get("description"),
                 individual_id=user_id
             )
             db.session.add(new_wishlist)
             db.session.commit()
-            return new_wishlist.to_dict(), 201
+            return new_wishlist.to_dict(), 201 
         except ValueError as e:
             return {"message": [str(e)]}
+
+
+
+        # user_id = json.get("user_id")
+        # user_info = IndividualModel.query.filter(IndividualModel.id == user_id).first()
+        # if user_info:
+        #     user_info = user_info.to_dict()
+        # else:
+        #     return {"error": "No user found"}
+        # print(user_info)
+
+        # try:
+        #     new_wishlist = WishlistModel(
+        #         name=json.get("name"),
+        #         img=json.get("img"),
+        #         description=json.get("description"),
+        #         individual_id=user_id
+        #     )
+        #     db.session.add(new_wishlist)
+        #     db.session.commit()
+        #     return new_wishlist.to_dict(), 201
+        # except ValueError as e:
+        #     return {"message": [str(e)]}
         
 
